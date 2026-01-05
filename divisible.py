6145ef88c6dfdeff405910ddce93c30eb0973daa
suvangkar-cmyk <suvangkar@gmail.com>
2026-01-05 18:06:58 +0600

    Add files via upload

---
+++ divisible.py
@@ -0,0 +1,8 @@
+print("Enter a number (Neumerator): ")
+numn =int(input())
+print("Enter a number (denominator)")
+numd = int(input())
+if numn%numd ==0:
+    print("divisible")
+else:
+    print("not divisible")
