2a66ebc19c987a3b5583d74158b9dce3f34d2b39
suvangkar-cmyk <suvangkar@gmail.com>
2026-01-05 17:51:11 +0600

    Add files via upload

---
+++ oporator.py
@@ -0,0 +1,13 @@
+v = 4
+w = 5
+x = 8
+y = 2
+z = 0
+z = (v+w) *x/y
+print("the answer is ", z )
+name="Alex"
+age=0
+if name =="Alex"or name =="Arnit"and age >=2:
+    print("Hi,Welcome")
+else:
+    print("Good bye")
