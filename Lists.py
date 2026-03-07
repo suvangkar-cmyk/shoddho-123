204308c649ddce2128d9a1c8df43df02c36a523a
suvangkar-cmyk <suvangkar@gmail.com>
2026-03-07 18:51:58 +0600

    Add files via upload

---
+++ Lists.py
@@ -0,0 +1,9 @@
+empty_list = []
+print()
+numbers = [1, 2 ,3, 4, 5]
+print(numbers)
+triples = [1, 2, 3]*3
+print(triples)
+ACSSOlist =[100, 200, 300, 400, 500]
+ACSSOlist = ACSSOlist[::-1]
+print(ACSSOlist,"\n")
