d3f3dc38d5fffb46a894e8cfe29aab67105aa508
suvangkar-cmyk <suvangkar@gmail.com>
2026-04-18 19:04:41 +0600

    Add files via upload

---
+++ Operations on Sets.py
@@ -0,0 +1,14 @@
+my_set = {1,2,3}
+print(my_set)
+my_set ={1,2,"Hello",(1,2,3)}
+print(my_set)
+my_set ={1,2,3,4,3,2}
+print(my_set)
+my_set = set([0,1,3,4,5])
+print(my_set)
+num_set = set([0,1,3,4,5])
+print("orginal set :")
+print(num_set)
+num_set.pop()
+print("After removing the frast elemant from the said set:")
+print(num_set,"\n")
